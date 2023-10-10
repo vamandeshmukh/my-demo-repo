3e3ff2ffbeeac9cfb53e8ac333668f958df5fd73
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-10 10:16:22 +0530

    conflict resolved 2

--- abhishek.js
+++ abhishek.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 console.log("My name is Abhishek Kumar Singh");
 
@@ -10,4 +10,7 @@ console.log('Renu');
 console.log("Shrish L Kulkarni");
 
 =======
->>>>>>> 9a806aa0bc4407af5bf204bea47672895f6a62c2
+=======
+console.log("My name is Abhishek Kumar Singh");
+console.log("My name is Shubh and i am Abhishek's best friend");
+
