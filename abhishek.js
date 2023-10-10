744f7315cfd7cbde4963c4a4a0a14aa1ff0cc0f2
Shubh Srivastava <66565506+Shubh30april@users.noreply.github.com>
2023-10-10 09:31:55 +0530

    Shubh's Commit

--- abhishek.js
+++ abhishek.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 console.log("My name is Abhishek Kumar Singh");
-=======
-console.log('Ayesha');
->>>>>>> b081c788172f9250992610992a8e6338418392c4
+console.log("My name is Shubh and i am Abhishek's best friend");
